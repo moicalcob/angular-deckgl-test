0d29581f310e537e8d01f02107339bebeccabc06
Moisés Calzado <mcalzado@cartodb.com>
2023-06-16 12:10:25 +0200

    Init repo

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CartoLayer, MAP_TYPES, colorContinuous } from '@deck.gl/carto';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-tour-of-heroes';
+}
